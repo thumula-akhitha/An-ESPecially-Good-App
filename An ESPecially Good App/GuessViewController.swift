8a7f80d27aa7af0ea411ed4926139bf0050dd02b
thumula-akhitha <akhitha.thumula@gmail.com>
2020-02-28 20:38:58 -0600

    Initial Commit

---
+++ An ESPecially Good App/GuessViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  An ESPecially Good App
+//
+//  Created by student on 2/28/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class GuessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ An ESPecially Good App/StatisticsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  An ESPecially Good App
+//
+//  Created by student on 2/28/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
